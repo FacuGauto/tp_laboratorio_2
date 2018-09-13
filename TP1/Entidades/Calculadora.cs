802856a48fffe4559075e411cf746b7f9f403d18
alumno <alumno>
2018-09-13 20:47:50 -0300

    primer commit

---
+++ TP1/Entidades/Calculadora.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        public double Operar(Numero num1,Numero num2,String operador)
+        {
+            double resultado;
+            operador = Calculadora.ValidarOperador(operador);
+
+            switch (operador)
+            {
+                case "+":
+                    resultado = num1 + num2;
+                    break;
+                case "-":
+                    resultado = num1 - num2;
+                    break;
+                case "*":
+                    resultado = num1 * num2;
+                    break;
+                case "/":
+                    resultado = num1 / num2;
+                    break;
+                default:
+                    break;
+            }
+            return resultado;
+        }
+
+        private static string ValidarOperador(string operador)
+        {
+            string[] operadores = {"+","-","/","*"};
+            int validacion;
+            for (int i = 0; i < operadores.Length; i++)
+            {
+                validacion = operador.CompareTo(operadores[i]);
+                if (validacion == 0)
+                {
+                    return operador;
+                }
+            }
+            return "+";
+        }
+    }
+}
---
+++ TP1/Entidades/Numero.cs
@@ -0,0 +1,46 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Numero
+    {
+        private double numero;
+
+        private double ValidarNumero(string strNumero)
+        {
+            double numero;
+            if(double.TryParse(strNumero,out numero))
+            {
+                return numero;
+            }
+            return 0;
+        }
+
+        public string BinarioDecimal(string binario)
+        {
+            int resultado = 0;
+            foreach (char b in binario)
+            {
+                if(b != '0' && b != '1')
+                {
+                    return "Valor invalido";
+                }
+            }
+            for (int i = 0; i < binario.Length; i++)
+            {
+                if (binario[i] == '1')
+                {
+                    resultado += (int)Math.Pow(2, i);
+                }
+            }
+            return resultado.ToString();
+        }
+
+        public DecimalBinario
+
+    }
+}
