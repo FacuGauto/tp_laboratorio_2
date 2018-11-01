2c8b3724640948f67660030784814b4722af1015
alumno <alumno>
2018-10-31 22:12:36 -0300

    segundo commit

---
+++ TP3/Gauto.Facundo.2ºC.TP3/Clases Abstractas/Profesor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_Abstractas
+{
+    public sealed class Profesor:Universitario
+    {
+        private Queue<EClases> clasesDelDia;
+    }
+}
--- TP3/Gauto.Facundo.2ºC.TP3/Clases Abstractas/Universitario.cs
+++ TP3/Gauto.Facundo.2ºC.TP3/Clases Abstractas/Universitario.cs
@@ -6,15 +6,18 @@ using System.Threading.Tasks;
 
 namespace Clases_Abstractas
 {
-    public abstract class Universitario :Persona
+    public abstract class Universitario : Persona
     {
         private int legajo;
 
         public Universitario()
         {
-
         }
 
+        public Universitario(int legajo,string nombre,string apellido,string dni,ENacionalidad nacionalidad) :base(nombre,apellido,dni,nacionalidad)
+        {
+            this.legajo = legajo;
+        }
         public override bool Equals(object obj)
         {
             return base.Equals(obj);
@@ -22,6 +25,18 @@ namespace Clases_Abstractas
         protected string MostrarDatos()
         {
             return "a";
+        }
+        protected string ParticiparEnClase()
+        {
+            return "";
+        }
+        public static bool operator ==(Universitario pg1,Universitario pg2)
+        {
+            return true;
+        }
+        public static bool operator !=(Universitario pg1, Universitario pg2)
+        {
+            return !(pg1 == pg2);
         }
     }
 }
