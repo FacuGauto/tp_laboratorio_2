c9f8ce83edb89318842ac988c0319c916f9b3e72
FacuGauto <fgauto1992@gmail.com>
2018-11-29 01:07:24 -0300

    test

---
+++ RecuperatoriosTP/TP3/Gauto.Facundo.2°C.TP3/TestValoresNulos/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestValoresNulos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestValoresNulos")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("be69ae13-7f2f-4c68-b32d-b1f84411d09c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RecuperatoriosTP/TP3/Gauto.Facundo.2°C.TP3/TestValoresNulos/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Clases_Abstractas;
+using Clases_Instanciables;
+
+namespace TestValoresNulos
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Alumno alumno;
+        Profesor profesor;
+        Universidad universidad;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            alumno = new Alumno(1, "Juan", "Lopez", "12234456", Persona.ENacionalidad.Argentino, Universidad.EClases.Programacion, Alumno.EEstadoCuenta.Becado);
+            profesor = new Profesor(2, "Roberto", "Juarez", "32234456", Persona.ENacionalidad.Argentino);
+            universidad = new Universidad();
+
+            Assert.IsNotNull(universidad.Alumnos);
+            Assert.IsNotNull(universidad.Instructores);
+            Assert.IsNotNull(universidad.Jornadas);
+            Assert.IsNotNull(alumno);
+            Assert.IsNotNull(profesor);
+
+        }
+    }
+}
